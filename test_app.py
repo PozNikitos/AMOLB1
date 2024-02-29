006d213556cb0314da76843543dd3b5a5d7081a8
nikita <nikita@ebiworks.com>
2024-02-29 16:43:57 +0300

    add unittest

--- test_app.py
+++ test_app.py
@@ -1,13 +1,18 @@
+import unittest
 from app import add
 
-def test_addition_1():
-    assert add(1,  2) ==  3
+class TestAddition(unittest.TestCase):
+    def test_addition_1(self):
+        self.assertEqual(add(1, 2), 3)
 
-def test_addition_2():
-    assert add(1,  2) !=  5
+    def test_addition_2(self):
+        self.assertNotEqual(add(1, 2), 5)
 
-def test_addition_3():
-    assert add(-1,  1) ==  0
+    def test_addition_3(self):
+        self.assertEqual(add(-1, 1), 0)
 
-def test_addition_4():
-    assert add(0,  0) ==  0
+    def test_addition_4(self):
+        self.assertEqual(add(0, 0), 0)
+
+if __name__ == '__main__':
+    unittest.main()
